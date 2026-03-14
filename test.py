889c43a50d1ad28f253fc4e63d0ed122c29d05d6
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-14 22:59:04 +0900

    feat: テストを実行するためのメイン関数を追加

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ import unittest
 class testSinValue(unittest.TestCase):
     def sin0(self):
         self.assertEqual(sinValue("sin0°"), "0")
+        
+if __name__ == "__main__":
+    unittest.main()
