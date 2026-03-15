5e714e81303f155cce206979657fc377129af962
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-15 11:57:17 +0900

    test: 無効な文字列に対しエラーを返すことのテストを追加

--- test.py
+++ test.py
@@ -12,6 +12,9 @@ class TestSinValue(unittest.TestCase):
         self.assertEqual(sin_value("sin60°"), "√3/2")
     def test_sin_90(self):
         self.assertEqual(sin_value("sin90°"), "1")
+    def test_undefined_tri_ratio(self):
+        with self.assertRaisesRegex(ValueError, "sin_value: a is not defined"):
+            sin_value("a")
         
 if __name__ == "__main__":
     unittest.main()
