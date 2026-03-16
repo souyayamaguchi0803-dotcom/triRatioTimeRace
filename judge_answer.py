287f75c66c10f9e7b6e3a6407559533691afd887
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-16 18:03:02 +0900

    docs: コメントを追加

--- judge_answer.py
+++ judge_answer.py
@@ -1,7 +1,7 @@
 from sin_value import sin_value
 
 def judge(question, answer):
-    if answer == sin_value(question):
+    if answer == sin_value(question): # 正解の場合
         return "Correct!"
-    else:
+    else: # 不正解の場合
         return "Oops!"
