a6071abfd237723a28e6092aa01726060d9beb9a
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-16 16:37:53 +0900

    refactor: 問題選択に使う関数をchoiceに変更

--- generate_question.py
+++ generate_question.py
@@ -1,9 +1,8 @@
-from random import randint
+from random import choice
 
 # 問題リスト
 QUESTIONS = ["sin0°", "sin30°", "sin45°", "sin60°", "sin90°"]
 
 # 問題を生成する関数
 def generate_question():
-    question_number = randint(0, 4)
-    return QUESTIONS[question_number]
+    return choice(QUESTIONS)
