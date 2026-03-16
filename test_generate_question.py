abff21dcdc735a50a8a2d6e5f09b89186c320517
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-16 16:36:36 +0900

    test: 乱数を用いるため、テストを100回繰り返す方式に変更

--- test_generate_question.py
+++ test_generate_question.py
@@ -3,7 +3,8 @@ from generate_question import generate_question, QUESTIONS
 
 class TestGenerateQuestion(unittest.TestCase):
     def test_question_is_in_questions(self):
-        self.assertIn(generate_question(), QUESTIONS)
+        for _ in range(100):
+	        self.assertIn(generate_question(), QUESTIONS)
         
 if __name__ == "__main__":
     unittest.main()
