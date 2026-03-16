885d64b55afd5478d6c644e5a272d26af0201ade
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-16 17:45:37 +0900

    test: 間違えた答えには"Oops!"と返すことのテストを追加

--- test_judge_answer.py
+++ test_judge_answer.py
@@ -4,6 +4,8 @@ from judge_answer import judge
 class TestJudgeAnswer(unittest.TestCase):
     def test_correct_answer(self):
         self.assertEqual(judge("sin0°", "0"), "Correct!")
+    def test_wrong_answer(self):
+        self.assertEqual(judge("sin0°", "1"), "Oops!")
 
 if __name__ == "__main__":
     unittest.main()
