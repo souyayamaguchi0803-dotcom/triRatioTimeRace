6a61fb310dfc49d27f1f19efc45d3cab4a1118d2
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-16 16:33:08 +0900

    docs: コメントを追加

--- generate_question.py
+++ generate_question.py
@@ -1,7 +1,9 @@
 from random import randint
 
+# 問題リスト
 QUESTIONS = ["sin0°", "sin30°", "sin45°", "sin60°", "sin90°"]
 
+# 問題を生成する関数
 def generate_question():
     question_number = randint(0, 4)
     return QUESTIONS[question_number]
