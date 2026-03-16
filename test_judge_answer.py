a312416cbe1ef7e7fcf63e84d8d17b894e485551
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-16 17:42:15 +0900

    test: 正しい答えに"Correct!"と返すというテストを追加

---
+++ test_judge_answer.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestJudgeAnswer(unittest.TestCase):
+    def test_correct_answer(self):
+        self.assertEqual(judge("sin0°", "0"), "Correct!")
+
+if __name__ == "__main__":
+    unittest.main()
