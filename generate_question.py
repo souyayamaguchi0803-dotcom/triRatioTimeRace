b75dd8d05ec008e63c95ae0e640a2e0afdd3d683
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-16 16:32:38 +0900

    refactor: 問題番号を変数に変更

--- generate_question.py
+++ generate_question.py
@@ -3,4 +3,5 @@ from random import randint
 QUESTIONS = ["sin0°", "sin30°", "sin45°", "sin60°", "sin90°"]
 
 def generate_question():
-    return QUESTIONS[randint(0, 4)]
+    question_number = randint(0, 4)
+    return QUESTIONS[question_number]
