176d60a059ac94af53f3a8cc7a059ff1842eed15
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-16 16:35:36 +0900

    refactor: テストをassertInを用いるものに変更

--- test_generate_question.py
+++ test_generate_question.py
@@ -3,7 +3,7 @@ from generate_question import generate_question, QUESTIONS
 
 class TestGenerateQuestion(unittest.TestCase):
     def test_question_is_in_questions(self):
-        self.assertTrue(generate_question() in QUESTIONS)
+        self.assertIn(generate_question(), QUESTIONS)
         
 if __name__ == "__main__":
     unittest.main()
