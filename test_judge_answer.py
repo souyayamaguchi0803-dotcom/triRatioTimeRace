49987e55ff6154656d57e88812991362a0248b5d
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-16 17:55:24 +0900

    test: subTestを利用し複数のケースをテストする方式に変更

--- test_judge_answer.py
+++ test_judge_answer.py
@@ -3,9 +3,21 @@ from judge_answer import judge
 
 class TestJudgeAnswer(unittest.TestCase):
     def test_correct_answer(self):
-        self.assertEqual(judge("sin0°", "0"), "Correct!")
+        params = [
+			("sin0°", "0"),
+            ("sin30°", "1/2")
+		]
+        for question, answer in params:
+            with self.subTest(question=question, answer=answer):
+                self.assertEqual(judge(question, answer), "Correct!")
     def test_wrong_answer(self):
-        self.assertEqual(judge("sin0°", "1"), "Oops!")
+        params = [
+			("sin0°", "1"),
+            ("sin30°", "1")
+		]
+        for question, answer in params:
+            with self.subTest(question=question, answer=answer):
+                self.assertEqual(judge(question, answer), "Oops!")
 
 if __name__ == "__main__":
     unittest.main()
