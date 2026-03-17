bf687c30e23d9b3def315e7c2e4b0fe167cba2e1
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-17 14:07:07 +0900

    refactor: 辞書内でCHOICESを使用し選択肢を記述

--- get_choice_value.py
+++ get_choice_value.py
@@ -1,11 +1,11 @@
 CHOICES = ["a", "b", "c", "d", "e"]
 
 VALUE_DICT = {
-	"a" : "0",
-    "b" : "1/2",
-    "c" : "√2/2",
-    "d" : "√3/2",
-    "e" : "1"
+	CHOICES[0] : "0",
+    CHOICES[1] : "1/2",
+    CHOICES[2] : "√2/2",
+    CHOICES[3] : "√3/2",
+    CHOICES[4] : "1"
 }
 
 def get_choice_value(choice):
