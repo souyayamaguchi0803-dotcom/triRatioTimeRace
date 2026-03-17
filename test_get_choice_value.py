f326efa7596c7acb671aaf2c1dfe6f9619745b18
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-17 14:16:59 +0900

    test: 無効な選択肢にはエラーを出すことのテストを追加

--- test_get_choice_value.py
+++ test_get_choice_value.py
@@ -5,6 +5,9 @@ class TestGetChoiceValue(unittest.TestCase):
     def test_get_correct_value(self):
         for choice in CHOICES:
             self.assertEqual(get_choice_value(choice), VALUE_DICT[choice])
+    def test_invalid_choice(self):
+        with self.assertRaisesRegex(ValueError, "get_choice_value: invalid choice"):
+            get_choice_value(".")
 
 if __name__ == "__main__":
     unittest.main()
