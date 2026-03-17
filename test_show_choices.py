13bcdd9b00c59ccb7f2116df6e3c8b83f95ef437
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-17 14:29:57 +0900

    test: 選択肢の一覧を返すことのテストを追加

---
+++ test_show_choices.py
@@ -0,0 +1,11 @@
+import unittest
+from choices import show_choices, CHOICES, VALUE_DICT
+
+class TestShowChoices(unittest.TestCase):
+    def test_show_chioces(self):
+        choices_list = show_choices()
+        for choice in CHOICES:
+            self.assertIn(f"{choice}. {VALUE_DICT}", choices_list)
+
+if __name__ == "__main__":
+    unittest.main()
