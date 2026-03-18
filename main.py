634dedd0fcfd19ff4dd3e131716a8a10653b0234
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-18 21:51:55 +0900

    feat: 選択肢を表示する機能を追加

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from generate_question import generate_question
+from choices import show_choices
 
 def main_game():
     question = generate_question()
     print(f"{question} = ?")
+    
+    choices = show_choices()
+    print(choices)
     
     
 if __name__ == "__main__":
