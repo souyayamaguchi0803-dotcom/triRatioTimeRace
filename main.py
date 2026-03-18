aa97a9b8c465ed1603c9e12a9c5fefcdfe5a090d
山口創也 <souyayamaguchi.0803@gmail.com>
2026-03-18 22:15:49 +0900

    feat: メインゲームの核部分を実装 (#11)
    - 問題表示機能
    - 選択肢表示機能
    - 解答受付機能
    - 結果表示機能
    
    ---------
    
    Co-authored-by: YamaguchiSouya <souyayamaguchi@gmail.com>

---
+++ main.py
@@ -0,0 +1,33 @@
+from generate_question import generate_question
+from choices import CHOICES, show_choices, is_valid_choice, get_choice_value
+from judge_answer import judge
+
+def main_game():
+    print()
+    
+    # 問題を表示
+    question = generate_question()
+    print(f"{question} = ?")
+    
+    # 選択肢を表示
+    choices = show_choices()
+    print(choices)
+    print()
+    
+    # 回答を取得
+    print("your answer > ", end="")
+    answer = input()
+    
+    # 正誤判定
+    if is_valid_choice(answer):
+        answer_value = get_choice_value(answer)
+        result = judge(question, answer_value)
+        print(result)
+    else:
+        print(f"please choice answer from {CHOICES[0]} ~ {CHOICES[-1]}.")
+    
+    print()
+    
+    
+if __name__ == "__main__":
+    main_game()
