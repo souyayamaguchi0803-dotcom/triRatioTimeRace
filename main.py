c4a8d93897d63ba62d7cd4dfea02208d92923352
YamaguchiSouya <souyayamaguchi@gmail.com>
2026-03-18 21:52:25 +0900

    docs: コメントを追加

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from generate_question import generate_question
 from choices import show_choices
 
 def main_game():
+    # 問題を表示
     question = generate_question()
     print(f"{question} = ?")
     
+    # 選択肢を表示
     choices = show_choices()
     print(choices)
     
