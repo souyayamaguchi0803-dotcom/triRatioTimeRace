5a15086011e2c489fc67cb28047daa650765d3e2
山口創也 <souyayamaguchi.0803@gmail.com>
2026-03-19 16:13:37 +0900

    feat: 結果表示機能を追加 (#13)
    - 正答率表示機能を追加
    - クリアタイム表示機能を追加
    
    ---------
    
    Co-authored-by: YamaguchiSouya <souyayamaguchi@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import time
 from generate_question import generate_question
 from choices import CHOICES, show_choices, is_valid_choice, get_choice_value
 from judge_answer import judge
 
 def main_game():
+    question_num = 0
     current_score = 0
     target_score = 5
+    
+    start_time = time.perf_counter()
 
     while current_score < target_score:
+        question_num += 1
+        
         print()
         
         # 問題を表示
@@ -35,6 +41,20 @@ def main_game():
             print(f"please choice answer from {CHOICES[0]} ~ {CHOICES[-1]}.")
     
     print()
+    
+    # 結果発表
+    print("=== result ===")
+    
+    # 正答率表示
+    print(f"accuracy: {current_score}/{question_num}")
+    
+    # クリアタイム表示
+    end_time = time.perf_counter()
+    clear_time = end_time - start_time
+    print(f"time: {clear_time:.2f}")
+    
+    print()
+    
     
 if __name__ == "__main__":
     main_game()
